f48573f77d7b1739d2b7b1a9011d980d4d9b6c28
Valentin <valentin@waldeck.dev>
2022-10-20 22:30:27 +0200

    Rework router

--- backend/lib/api/main.dart
+++ backend/lib/api/main.dart
@@ -3,7 +3,7 @@ import 'package:shelf/shelf_io.dart' as shelf_io;
 import 'package:prcup_backend/api/router.dart';
 
 void main() async {
-  var router = APIRouter().router;
+  var router = MainRouter().router;
 
   var server = await shelf_io.serve(router, 'localhost', 8080);
 
--- backend/lib/api/router.dart
+++ backend/lib/api/router.dart
@@ -2,6 +2,14 @@ import 'package:shelf_router/shelf_router.dart';
 
 import 'package:prcup_backend/api/handlers/saves.dart';
 
+class MainRouter {
+  final router = Router();
+
+  MainRouter() {
+    router.mount('/prcup-api', APIRouter().router);
+  }
+}
+
 class APIRouter {
   final router = Router();
 
