e741c299353bcefd9799b467be85af5887aece24
Valentin <valentin@waldeck.dev>
2024-02-24 21:42:35 +0100

    feat: add types

--- src/types.ts
+++ src/types.ts
@@ -1,22 +1,3 @@
-export type User = {
-  avatar_url: string;
-  html_url: string;
-  login: string;
-};
-
-export type Pull = {
-  number: number;
-  merged: boolean;
-  user: User;
-};
-
-export type Issue = {
-  number: number;
-  state_reason: "not_planned" | "completed" | null;
-  assignee: User;
-  user: User;
-};
-
 export const ItemTypeEnum = {
   PULL: "pull",
   ISSUE: "issue",
@@ -27,5 +8,28 @@ export type ItemType = typeof ItemTypeEnum[keyof typeof ItemTypeEnum];
 export type Score = {
   number: number;
   type: ItemType;
+  user: User;
+  state: Pull["merged"] | Issue["state_reason"];
+  score: number;
+};
+
+export type User = {
+  avatar_url: string;
+  html_url: string;
+  login: string;
+};
+
+export type Pull = {
+  type: typeof ItemTypeEnum.PULL;
+  number: number;
+  merged: boolean;
+  user: User;
+};
+
+export type Issue = {
+  type: typeof ItemTypeEnum.ISSUE;
+  number: number;
+  state_reason: "not_planned" | "completed" | null;
+  assignee: User;
   user: User;
 };
