ca69de66d602d9a1c68dce91c35bd973ebaf3db6
Valentin <valentin@waldeck.dev>
2024-03-09 17:58:33 +0100

    feat: add generator

---
+++ ui/generator.ts
@@ -0,0 +1,72 @@
+import { nunjucks, path } from "../dep.ts";
+import { UserScores } from "../src/types.ts";
+
+const BASE_DIR = path.join(Deno.cwd(), "ui");
+const TEMPLATE_DIR = path.join(BASE_DIR, "templates");
+const OUTPUT_DIR = path.join(BASE_DIR, "out");
+
+type TEMPLATES = "scores.njk";
+
+export class PageGenerator {
+  constructor(
+    private repository: string,
+    private templateDir: string = TEMPLATE_DIR,
+    private outDir: string = OUTPUT_DIR,
+  ) {
+  }
+
+  private writePage(filePath: string, content: string) {
+    Deno.writeTextFileSync(filePath, content);
+  }
+
+  public generatePage(
+    template: TEMPLATES,
+    data: { rankedScores: UserScores },
+  ) {
+    const renderedTemplate = nunjucks.render(
+      path.join(this.templateDir, template),
+      { rankedScores: this.prepareScoreData(data.rankedScores) },
+    );
+    this.writePage(
+      path.join(this.outDir, `${this.repository}.html`),
+      renderedTemplate,
+    );
+    console.info(
+      `Generated template "${template}" for repository "${this.repository}"`,
+    );
+  }
+
+  private prepareScoreData(data: UserScores) {
+    const newData: UserScores = new Map();
+
+    for (const [position, items] of data.entries()) {
+      const newItems = [];
+
+      for (const item of items) {
+        newItems.push({
+          user: item.user,
+          emoji: this.getEmoji(position),
+          totalScore: item.scores.reduce((sum, score) => sum + score.score, 0),
+          scores: item.scores,
+        });
+      }
+
+      newData.set(position, newItems);
+    }
+
+    return newData;
+  }
+
+  private getEmoji(position: number): string | null {
+    switch (position) {
+      case 1:
+        return "🏆";
+      case 2:
+        return "🥈";
+      case 3:
+        return "🥉";
+      default:
+        return null;
+    }
+  }
+}
